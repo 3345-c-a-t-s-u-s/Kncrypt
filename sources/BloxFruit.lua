69b46b4b46e687448faa02750cd63b367a897f5a
4lpaca <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2025-01-07 18:45:35 +0700

    Update BloxFruit.lua

--- sources/BloxFruit.lua
+++ sources/BloxFruit.lua
@@ -19,11 +19,45 @@ win.AddButton('Get Key',function()
 end);
 
 win.AddButton('Login',function(a)
-    a.Close()
+    task.delay(1,function() a.Close(); end);
+    
     script_key=a.Text;
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/84540224acb21db4b7ffadb76a63fefd.lua"))()
+    
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/84540224acb21db4b7ffadb76a63fefd.lua"))();
 end);
 
 win.AddButton('Discord',function()
     (setclipboard or toclipboard)('https://discord.gg/kncrypthub');
+
+    if request then
+			request({
+				Url = 'http://127.0.0.1:6463/rpc?v=1',
+				Method = 'POST',
+				Headers = {
+					['Content-Type'] = 'application/json',
+					Origin = 'https://discord.com'
+				},
+				Body = game:GetService('HttpService'):JSONEncode({
+					cmd = 'INVITE_BROWSER',
+					nonce = game:GetService('HttpService'):GenerateGUID(false),
+					args = {code = 'wPaQZwhMHy'}
+				})
+			})
+	end
 end);
+
+if request then
+			request({
+				Url = 'http://127.0.0.1:6463/rpc?v=1',
+				Method = 'POST',
+				Headers = {
+					['Content-Type'] = 'application/json',
+					Origin = 'https://discord.com'
+				},
+				Body = game:GetService('HttpService'):JSONEncode({
+					cmd = 'INVITE_BROWSER',
+					nonce = game:GetService('HttpService'):GenerateGUID(false),
+					args = {code = 'wPaQZwhMHy'}
+				})
+			})
+	end
