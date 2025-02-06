1cc02c19ff0d533af23e64381ab5d7529c6ff657
4lpaca <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2025-02-06 11:19:17 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,10 @@
 local st,kl,tg,ka = loadstring,rawget,game.HttpGet,"https://raw.githubusercontent.com/3345-c-a-t-s-u-s/Kncrypt/refs/heads/main/sources/";
 local Ua,ctx,dir = setmetatable({[994732206]="BloxFruit.lua",[2440500124]="Doors.lua",[4367208330]="Pressure.lua",[5750914919]="Fisch.lua"},{__newindex = function(self,a,d)print('failed!');end,__metatable="The metatable is locked"}),game.GameId,error;
 if kl(Ua,ctx) then
-     st(tg(game,ka..kl(Ua,ctx)))({auth=true,RunOnLoader=true,Version="0.1.1"});
+     if not getgenv().fastfetch then
+          return st(tg(game,ka..kl(Ua,ctx)))({auth=true,RunOnLoader=true,Version="0.1.1"});
+     end;
+     return kl(Ua,ctx);
 else
     dir("Failed to Execute\nUnsupported");
 end;
