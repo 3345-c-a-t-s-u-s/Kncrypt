c2525ec1f7f66f6019f9ca8d7e2d367509c5cc38
4lpaca <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2026-02-22 11:01:32 +0700

    Update Fisch.lua

--- sources/Fisch.lua
+++ sources/Fisch.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b15494659f43149a2ff502093aeb645.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/4lpaca-pin/Arceney/refs/heads/main/main.luau"))()
